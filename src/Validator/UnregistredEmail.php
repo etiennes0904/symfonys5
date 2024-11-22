c45af969b4796dce88dbeb7cb64f146b68c805f0
Etienne Sautivet <124677737+etiennes0904@users.noreply.github.com>
2024-11-22 11:42:55 +0100

    feat: add Validator

---
+++ src/Validator/UnregistredEmail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute]
+class UnregistredEmail extends Constraint
+{
+    public string $message = 'L\'email "{{ string }}" est déjà lié à un utilisateur.';
+
+    // all configurable options must be passed to the constructor
+    public function __construct(?string $message = null, ?array $groups = null, $payload = null)
+    {
+        parent::__construct([], $groups, $payload);
+
+        $this->message = $message ?? $this->message;
+    }
+}
